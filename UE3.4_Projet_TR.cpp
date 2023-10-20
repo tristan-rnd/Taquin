7fdcbe335a42cfd192b567fc90b279e73c2320c3
tristan-rnd <107555726+tristan-rnd@users.noreply.github.com>
2023-10-20 17:05:38 +0200

    Update UE3.4_Projet_TR.cpp

--- UE3.4_Projet_TR.cpp
+++ UE3.4_Projet_TR.cpp
@@ -1,16 +1,18 @@
 #include "Plateau.h"
+#include <fstream>
 
 int main(int argc, char* argv[])
 {
 
-	Plateau plateau(4,200);
+	Plateau plateau(4, 200);
 
-	plateau.afficher();
+	plateau.initialise_PlateauITK2VTK();
 	plateau.randomise();
-	plateau.afficher();
-	plateau.est_victorieux();
-	plateau.solution_from_randomise();
-	plateau.afficher();
+
+	std::vector<std::string> _chemin_solution{ plateau.get_Solution() };
+	std::ofstream out("output.txt");
+	for(const auto& i:_chemin_solution)	out << i;
+	out.close();
 	plateau.est_victorieux();
 	plateau.initialise_affichageVTK();
 
